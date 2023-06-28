20caa2f526fa51bef223e27d85512dfffdbfb9d1
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-06-28 11:57:17 +0500

    HUNZA

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <div className="bg-[url('https://res.cloudinary.com/dc367rgig/image/upload/v1687935376/MacBook_Pro_14__-_1_hud3bw.png')] h-screen bg-center p-2 bg-cover md:w-full bg-no-repeat ">
+      <div className="bg-[url('https://res.cloudinary.com/dc367rgig/image/upload/v1687935376/MacBook_Pro_14__-_1_hud3bw.png')] bg-fixed h-screen bg-center p-2 bg-cover md:w-full bg-no-repeat ">
         <h1 className=" text-center p-2 font-CG text-white text-5xl font-semibold  md:text-9xl">
           This is not
           <br /> Iceland!
