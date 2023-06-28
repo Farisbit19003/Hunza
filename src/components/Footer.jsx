eccba93913b10a5fe8bc8af53713e9fe8ec9ff10
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-06-28 11:29:40 +0500

    HUNZA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="font-CG  text-7xl ml-5 text-[#1890d7] p-3">Hunza</div>
         <div className='flex flex-col justify-end mb-4 text-[#1890d7] text-end'>
             <div>
-                <p>+9231-6498024</p>
-                <p>contact@hunza.com</p>
-                <p>Copyright 2023</p>
+                <p className='text-lg font-light'>+9231-6498024</p>
+                <p className='text-lg mb-2 font-light'>contact@hunza.com</p>
+                <p className='text-sm font-light'>@Copyright 2023</p>
             </div>
         </div>
       </div>
